64c84451c76cb93ad2daf8c713b689e194156aec
gregorymartin <77732347+gregorymartin200261@users.noreply.github.com>
2022-09-02 14:33:03 +0200

    ex_rev_rentrée_2022 add

---
+++ ex_rev_rentrée_2022/ex_rev_rentrée_2022/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex_rev_rentrée_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            methode utils = new methode();
+            for (int i = 0; i < 10; i++)
+            {
+                double[] UserEnter = new double[10];
+                utils.Lecture(question)
+            }
+            
+        }
+    }
+}
---
+++ ex_rev_rentrée_2022/ex_rev_rentrée_2022/methode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex_rev_rentrée_2022
+{
+    public struct methode
+    {
+        static double lecture(string question)
+        {
+            Console.WriteLine(question);
+            string RepUser = Console.ReadLine();
+
+            double x;
+
+            while (!double.TryParse(RepUser ,out x)) 
+	        {
+                Console.WriteLine("Merci de rentrer des chiffres.");
+                RepUser = Console.ReadLine();
+            }
+
+            return x;
+        }
+    }
+}
